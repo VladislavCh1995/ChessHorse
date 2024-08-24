0705af1bac056dad970ec8a1022c5ce256642bfb
Vladislav Churilov <Nawaharjan@gmail.com>
2024-08-24 17:44:21 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessHorse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            //if (x2 != x1 && x2 <= (x1 + 2) && x2 > (x1 - 2))
+            //{
+                int spaceX = Math.Abs(x2 - x1);
+                int spaceY = Math.Abs(y2 - y1);
+                if ((spaceX == 2 && spaceY == 1) || (spaceX == 1 && spaceY == 2))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            //}
+            //else
+           // {
+           //     Console.WriteLine("NO");
+           // }
+        }
+    }
+}
